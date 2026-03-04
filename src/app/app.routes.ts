dd671ad2a4466f52650ab56b61f2b92b300b3630
LeksoKashia <lekso.kashia.1@btu.edu.ge>
2026-03-04 18:26:06 +0400

    new features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,38 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/landing/landing.component').then(m => m.LandingComponent) },
-  { path: 'services', loadComponent: () => import('./pages/services/services.component').then(m => m.ServicesComponent) },
-  { path: 'about', loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent) },
-  { path: 'contact', loadComponent: () => import('./pages/contact/contact.component').then(m => m.ContactComponent) },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/landing/landing.component').then(
+        (m) => m.LandingComponent,
+      ),
+  },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./pages/services/services.component').then(
+        (m) => m.ServicesComponent,
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(
+        (m) => m.ContactComponent,
+      ),
+  },
+  {
+    path: 'technologies',
+    loadComponent: () =>
+      import('./pages/technologies/technologies.component').then(
+        (m) => m.TechnologiesComponent,
+      ),
+  },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/pages/technologies/technologies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-technologies',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './technologies.component.html',
+  styleUrl: './technologies.component.scss',
+})
+export class TechnologiesComponent {}
